14af04299d2ebe04be710cf4d082466942811529
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-12-10 01:33:24 +0200

    Added common passwords map and password encryption before storing in db

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 require('dotenv').config()
 const fs = require('fs')
 const { Pool } = require('pg')
-const path = require('path');
+const path = require('path')
+const commonPasswordsMap = FillCommonPasswordsMap()
+
+function FillCommonPasswordsMap()
+{
+  const result = new Map()
+
+  const commonPass = fs.readFileSync(process.env.COMMON_PASSWORDS, "utf8")
+  const commonPassArr = commonPass.split('\n')
+  commonPassArr.forEach(pass => {
+    result.set(pass, 1)    
+  });
+
+  return result;
+}
 
 const fastify = require('fastify')({
   logger: true,
@@ -11,11 +25,6 @@ const fastify = require('fastify')({
     cert: fs.readFileSync(process.env.CERT),
     ca: [fs.readFileSync(process.env.CA)]
   }
-})
-
-const pool = new Pool({
-  connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
-  ssl: false
 })
 
 fastify
@@ -36,6 +45,11 @@ fastify
     reply.status(200).sendFile('index.html');
   })
   .post('/login', async (request, reply) => {
+    const pool = new Pool({
+      connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+      ssl: false
+    })
+
     const _sodium = require('libsodium-wrappers');
     (async () => {
       await _sodium.ready;
@@ -48,17 +62,21 @@ fastify
         reply.status(400).send({ status: 'password is too long, 60 chars max' })
         return;
       }
-
+      
       pool
-        .query(`select * from crypto_lab_5 where username=${username};`)
+        .query(`select * from crypto_lab_5 where username='${username}';`)
         .then(res => {
-          console.log(res)
-
           if (res.rows.length == 0)
             reply.status(403).send({ status: `failed to log in user ${username}: not found` })
 
-          const hashed_password = res.rows[0].password
+          const hashed_password = decrypt_after_extracting_nonce(
+            sodium,
+            res.rows[0].password,
+            sodium.from_hex(process.env.SECRET_KEY)
+          )
+          
           const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
+
           if (passwords_match)
             reply.status(200).send({ status: `successfully logged in user ${username}` })
           else
@@ -74,6 +92,11 @@ fastify
     })();
   })
   .post('/register', async (request, reply) => {
+    const pool = new Pool({
+      connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+      ssl: false
+    })
+
     const _sodium = require('libsodium-wrappers');
     (async () => {
       await _sodium.ready;
@@ -86,13 +109,20 @@ fastify
         reply.status(400).send({ status: 'password is too long, 60 chars max' })
         return;
       }
+      else if (commonPasswordsMap.has(password)) {
+        reply.status(400).send({ status: 'password is too simple, add some complexity to it' })
+        return;
+      }
 
-      const hashed_password = sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE)
+      const ciphered_password = encrypt_and_prepend_nonce(
+        sodium,
+        sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE),
+        sodium.from_hex(process.env.SECRET_KEY)
+      )
 
       pool
-        .query(`insert into crypto_lab_5 values (\'${username}\',\'${hashed_password}\');`)
+        .query(`insert into crypto_lab_5 values (\'${username}\',\'${ciphered_password}\');`)
         .then(res => {
-          console.log(res)
           reply.status(200).send({ status: `successfully registered user ${username}` })
         })
         .catch(err => {
@@ -114,3 +144,22 @@ const start = async () => {
   }
 }
 start()
+
+function encrypt_and_prepend_nonce(sodium, message, key) {
+  const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES)
+  const cipher = sodium.crypto_secretbox_easy(message, nonce, key)
+  return sodium.to_base64(new Uint8Array([...nonce, ...cipher]))
+}
+
+function decrypt_after_extracting_nonce(sodium, nonce_and_ciphertext_base64, key) {
+  const nonce_and_ciphertext = sodium.from_base64(nonce_and_ciphertext_base64)
+
+  if (nonce_and_ciphertext.length < sodium.crypto_secretbox_NONCEBYTES + sodium.crypto_secretbox_MACBYTES) {
+    throw "Short message"
+  }
+
+  const nonce = nonce_and_ciphertext.slice(0, sodium.crypto_secretbox_NONCEBYTES)
+  const ciphertext = nonce_and_ciphertext.slice(sodium.crypto_secretbox_NONCEBYTES)
+
+  return sodium.to_string(sodium.crypto_secretbox_open_easy(ciphertext, nonce, key))
+}
