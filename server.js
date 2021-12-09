beec6775cf63cc754216d4afd098a6aee400af7b
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-12-09 14:34:50 +0200

    Added hash

--- server.js
+++ server.js
@@ -1,26 +1,76 @@
+require('dotenv').config()
 const fs = require('fs')
-const path = require('path')
-const fastifyCors = require('fastify-cors')
+const path = require('path');
 
 const fastify = require('fastify')({
-    logger: true,
-    // https: {
-    //     key: fs.readFileSync('./tls/cert.key'),
-    //     cert: fs.readFileSync('./tls/cert.pem')
-    // }
+  logger: true,
+  http2: true,
+  https: {
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CERT),
+    ca: [fs.readFileSync(process.env.CA)]
+  }
 })
 
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname),
-  default: '/'
-}).register(fastifyCors, {
-  origin: '*',
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders:
-    'Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With,application/x-www-form-urlencoded'
-}).get('/', async (request, reply) => {
-  reply.status(200).sendFile('index.html');
-})
+fastify
+  .register(require('fastify-static'), {
+    root: path.join(__dirname),
+    default: '/'
+  })
+  .register(require('fastify-cors'), {
+    origin: '*',
+    methods: 'GET,POST',
+    allowedHeaders:
+      'Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With,application/x-www-form-urlencoded'
+  })
+  .register(require('fastify-formbody'))
+
+fastify
+  .get('/', async (request, reply) => {
+    reply.status(200).sendFile('index.html');
+  })
+  .post('/login', async (request, reply) => {
+    const _sodium = require('libsodium-wrappers');
+    (async () => {
+      await _sodium.ready;
+      const sodium = _sodium;
+
+      const username = request.body.username
+      const password = request.body.password
+
+      if (password.length > 60) {
+        reply.status(400).send({ status: 'password is too long, 60 chars max' })
+        return;
+      }
+
+      const hashed_password = sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE)
+      
+      const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
+      if (passwords_match)
+        reply.status(200).send({ status: 'successfully logged in' })
+      else
+        reply.status(403).send({ status: 'log in failed: incorrect password' })
+    })();
+  })
+  .post('/register', async (request, reply) => {
+    const _sodium = require('libsodium-wrappers');
+    (async () => {
+      await _sodium.ready;
+      const sodium = _sodium;
+
+      const username = request.body.username
+      const password = request.body.password
+
+      if (password.length > 60) {
+        reply.status(400).send({ status: 'password is too long, 60 chars max' })
+        return;
+      }
+      
+      const hashed_password = sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE)
+
+      reply.status(200).send({ status: 'successfully registered' })
+    })();
+  })
 
 const start = async () => {
   try {
