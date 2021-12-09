dc4846b3e288370edb1c06b8b211a45667141bef
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-12-09 15:16:01 +0200

    Added db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const fs = require('fs')
+const { Pool } = require('pg')
 const path = require('path');
 
 const fastify = require('fastify')({
@@ -12,6 +13,11 @@ const fastify = require('fastify')({
   }
 })
 
+const pool = new Pool({
+  connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+  ssl: false
+})
+
 fastify
   .register(require('fastify-static'), {
     root: path.join(__dirname),
@@ -21,7 +27,7 @@ fastify
     origin: '*',
     methods: 'GET,POST',
     allowedHeaders:
-      'Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With,application/x-www-form-urlencoded'
+      'Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With'
   })
   .register(require('fastify-formbody'))
 
@@ -43,13 +49,28 @@ fastify
         return;
       }
 
-      const hashed_password = sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE)
-      
-      const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
-      if (passwords_match)
-        reply.status(200).send({ status: 'successfully logged in' })
-      else
-        reply.status(403).send({ status: 'log in failed: incorrect password' })
+      pool
+        .query(`select * from crypto_lab_5 where username=${username};`)
+        .then(res => {
+          console.log(res)
+
+          if (res.rows.length == 0)
+            reply.status(403).send({ status: `failed to log in user ${username}: not found` })
+
+          const hashed_password = res.rows[0].password
+          const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
+          if (passwords_match)
+            reply.status(200).send({ status: `successfully logged in user ${username}` })
+          else
+            reply.status(403).send({ status: 'log in failed: incorrect password' })
+        })
+        .catch(err => {
+          console.log(err)
+          reply.status(403).send({ status: `failed to log in user ${username}` })
+        })
+        .finally(
+          pool.end()
+        )
     })();
   })
   .post('/register', async (request, reply) => {
@@ -65,10 +86,22 @@ fastify
         reply.status(400).send({ status: 'password is too long, 60 chars max' })
         return;
       }
-      
+
       const hashed_password = sodium.crypto_pwhash_str(password, sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE)
 
-      reply.status(200).send({ status: 'successfully registered' })
+      pool
+        .query(`insert into crypto_lab_5 values (\'${username}\',\'${hashed_password}\');`)
+        .then(res => {
+          console.log(res)
+          reply.status(200).send({ status: `successfully registered user ${username}` })
+        })
+        .catch(err => {
+          console.log(err)
+          reply.status(401).send({ status: `failed to register user ${username}` })
+        })
+        .finally(
+          pool.end()
+        )
     })();
   })
 
