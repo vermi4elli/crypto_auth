e1b81af38481825730e4f6527b3e22bcf088cee9
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-12-10 20:11:13 +0200

    Added envelope encryption for sensitive user data

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ require('dotenv').config()
 const fs = require('fs')
 const { Pool } = require('pg')
 const path = require('path')
+const { rows } = require('pg/lib/defaults')
 const commonPasswordsMap = FillCommonPasswordsMap()
 
-function FillCommonPasswordsMap()
-{
+function FillCommonPasswordsMap() {
   const result = new Map()
 
   const commonPass = fs.readFileSync(process.env.COMMON_PASSWORDS, "utf8")
   const commonPassArr = commonPass.split('\n')
   commonPassArr.forEach(pass => {
-    result.set(pass, 1)    
+    result.set(pass, 1)
   });
 
   return result;
@@ -62,7 +62,7 @@ fastify
         reply.status(400).send({ status: 'password is too long, 60 chars max' })
         return;
       }
-      
+
       pool
         .query(`select * from crypto_lab_5 where username='${username}';`)
         .then(res => {
@@ -74,13 +74,131 @@ fastify
             res.rows[0].password,
             sodium.from_hex(process.env.SECRET_KEY)
           )
-          
+
           const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
 
           if (passwords_match)
             reply.status(200).send({ status: `successfully logged in user ${username}` })
           else
             reply.status(403).send({ status: 'log in failed: incorrect password' })
+        })
+        .catch(err => {
+          console.log(err)
+          reply.status(403).send({ status: `failed to log in user ${username}` })
+        })
+        .finally(
+          pool.end()
+        )
+    })();
+  })
+  .post('/data', async (request, reply) => {
+    const pool = new Pool({
+      connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+      ssl: false
+    })
+
+    const _sodium = require('libsodium-wrappers');
+    (async () => {
+      await _sodium.ready;
+      const sodium = _sodium;
+
+      const username = request.body.username
+      const password = request.body.password
+
+      if (password.length > 60) {
+        reply.status(400).send({ status: 'password is too long, 60 chars max' })
+        return;
+      }
+
+      pool
+        .query(`select * from crypto_lab_5 where username='${username}';`)
+        .then(res => {
+          if (res.rows.length == 0)
+            reply.status(403).send({ status: `failed to log in user ${username}: not found` })
+
+          const hashed_password = sodium.to_string(decrypt_after_extracting_nonce(
+            sodium,
+            res.rows[0].password,
+            sodium.from_hex(process.env.SECRET_KEY)
+          ))
+
+          const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
+
+          if (passwords_match) {
+            const card_data = GetDecryptedData(sodium, res.rows[0].dek, res.rows[0].card)
+            const decoded_card_data = new TextDecoder().decode(card_data);
+            reply.status(200).send({ status: `successfully retrived data from user ${username}`, data: decoded_card_data })
+          }
+          else
+            reply.status(403).send({ status: 'log in failed: incorrect password' })
+        })
+        .catch(err => {
+          console.log(err)
+          reply.status(403).send({ status: `failed to log in user ${username}` })
+        })
+        .finally(
+          pool.end()
+        )
+    })();
+  })
+  .post('/update', async (request, reply) => {
+    const pool = new Pool({
+      connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+      ssl: false
+    })
+
+    const _sodium = require('libsodium-wrappers');
+    (async () => {
+      await _sodium.ready;
+      const sodium = _sodium;
+
+      const username = request.body.username
+      const password = request.body.password
+      const card_data = request.body.card
+
+      if (password.length > 60) {
+        reply.status(400).send({ status: 'password is too long, 60 chars max' })
+        return;
+      }
+
+      pool
+        .query(`select * from crypto_lab_5 where username='${username}';`)
+        .then(res => {
+          if (res.rows.length == 0)
+            reply.status(403).send({ status: `failed to log in user ${username}: not found` })
+
+          const hashed_password = sodium.to_string(decrypt_after_extracting_nonce(
+            sodium,
+            res.rows[0].password,
+            sodium.from_hex(process.env.SECRET_KEY)
+          ))
+
+          const passwords_match = sodium.crypto_pwhash_str_verify(hashed_password, password)
+
+          if (passwords_match) {
+
+            const result = GetEncryptedDataAndNewDek(sodium, card_data)
+
+            const pool = new Pool({
+              connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+              ssl: false
+            })
+
+            pool
+              .query(`update crypto_lab_5 set card='${result.card_data}',dek='${result.user_dek}' where username='${username}';`)
+              .then(res => {
+                reply.status(200).send({ status: `successfully updated data for user ${username}` })
+              })
+              .catch(err => {
+                console.log(err)
+                reply.status(403).send({ status: `failed to update data for user ${username}` })
+              })
+              .finally(
+                pool.end()
+              )
+          }
+          else
+            reply.status(403).send({ status: `failed to update data for user ${username}: incorrect data` })
         })
         .catch(err => {
           console.log(err)
@@ -104,6 +222,7 @@ fastify
 
       const username = request.body.username
       const password = request.body.password
+      const card_data = request.body.card
 
       if (password.length > 60) {
         reply.status(400).send({ status: 'password is too long, 60 chars max' })
@@ -120,8 +239,10 @@ fastify
         sodium.from_hex(process.env.SECRET_KEY)
       )
 
+      const result = GetEncryptedDataAndNewDek(sodium, card_data)
+
       pool
-        .query(`insert into crypto_lab_5 values (\'${username}\',\'${ciphered_password}\');`)
+        .query(`insert into crypto_lab_5 values ('${username}','${ciphered_password}','${result.card_data}','${result.user_dek}');`)
         .then(res => {
           reply.status(200).send({ status: `successfully registered user ${username}` })
         })
@@ -161,5 +282,38 @@ function decrypt_after_extracting_nonce(sodium, nonce_and_ciphertext_base64, key
   const nonce = nonce_and_ciphertext.slice(0, sodium.crypto_secretbox_NONCEBYTES)
   const ciphertext = nonce_and_ciphertext.slice(sodium.crypto_secretbox_NONCEBYTES)
 
-  return sodium.to_string(sodium.crypto_secretbox_open_easy(ciphertext, nonce, key))
+  return sodium.crypto_secretbox_open_easy(ciphertext, nonce, key)
+}
+
+function GetEncryptedDataAndNewDek(sodium, data) {
+  const user_dek = sodium.crypto_secretbox_keygen()
+  const ciphered_card_data = encrypt_and_prepend_nonce(
+    sodium,
+    data,
+    user_dek
+  )
+  const ciphered_user_dek = encrypt_and_prepend_nonce(
+    sodium,
+    user_dek,
+    sodium.from_hex(process.env.SECRET_KEY)
+  )
+
+  this.card_data = ciphered_card_data
+  this.user_dek = ciphered_user_dek
+
+  return this
+}
+
+function GetDecryptedData(sodium, dek, card_data) {
+  const user_dek = decrypt_after_extracting_nonce(
+    sodium,
+    dek,
+    sodium.from_hex(process.env.SECRET_KEY)
+  )
+  const card_data_deciphered = decrypt_after_extracting_nonce(
+    sodium,
+    card_data,
+    user_dek
+  )
+  return card_data_deciphered
 }
